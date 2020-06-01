5aae35e0ebbf0b996284ee752c3caebf27bde3a1
Sanjeet Sahay <sanjeet@earthrise.local>
2020-05-31 22:38:58 -0700

    deps

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from "react";
+import Amplify from "aws-amplify";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import "@aws-amplify/ui/dist/style.css";
+import Amplify from "aws-amplify";
 
 function App() {
   return <h1>Hello, admin 🚀</h1>;
